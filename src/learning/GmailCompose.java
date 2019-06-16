d0b12a6bdcb14150e6071e402f4dcfc2f506f451
maheshfita <imbabu25@gmail.com>
2019-06-16 13:29:18 +0530

    added new line

--- src/learning/GmailCompose.java
+++ src/learning/GmailCompose.java
@@ -11,6 +11,7 @@ public class GmailCompose {
 		
 		System.setProperty("webdriver.chrome.driver", "E:/Mahesh/ChromeDriver/chromedriver_win32/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
+		// sdgdfsgsdgfdsf
 		driver.get("https://accounts.google.com/ServiceLogin/signinchooser?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin");
 		
 		
